61e8a4050186b1b9e9c8647b37a0678c02ad7091
Felipe <pipeba1408@gmail.com>
2024-10-09 16:04:29 -0500

    Initial commit

---
+++ src/main/java/com/patrones/kafkaordering/KafkaOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.patrones.kafkaordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaOrderingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patrones/kafkaordering/KafkaOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrones.kafkaordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
