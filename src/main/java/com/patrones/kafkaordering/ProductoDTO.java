e6eca98385dc41f38ab6d3378738020af5755d74
Felipe <pipeba1408@gmail.com>
2024-10-09 20:10:35 -0500

    Cambia formato

--- src/main/java/com/patrones/kafkaordering/ProductoDTO.java
+++ src/main/java/com/patrones/kafkaordering/ProductoDTO.java
@@ -2,5 +2,7 @@ package com.patrones.kafkaordering;
 
 import java.math.BigDecimal;
 
-public record ProductoDTO(Integer id, String nombre, BigDecimal valor) {
-}
+public record ProductoDTO(
+        Integer id,
+        String nombre,
+        BigDecimal valor) { }
